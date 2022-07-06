5eff942860a921501e378a0b8b76e0da6f67abac
Julia Sousa <60710752+eleph-antastic@users.noreply.github.com>
2022-07-05 22:14:23 -0700

    Update
    closure/wrap for buttons is working, unfortunately text field does not take a value that int conversion can understand (base10).
    Update to fix:
    trying to find an alternative so min and max ranges can be set as int's

--- PythonforMayaExploration.py
+++ PythonforMayaExploration.py
@@ -11,94 +11,84 @@ def transfer_animation(obj_one, obj_two, cut=False):
     cmds.pasteKey(obj_two, animation='objects', option='replaceCompletely')
 """
 #closure with mini environment
-
-def wrap_function(function_title, min_range, max_range):
-    def randomize_translate_all():
-        #Randomizes Translations of All Selected objects
-        for i in cmds.ls(sl=True):
-            for a in ['X', 'Y', 'Z']:
-                randnum = random.uniform(min_range, max_range)
-                cmds.setAttr('{}.translate{}'.format(i, a), randnum)
-        pass
-                  
-    def randomize_translate():
-        #Randomizes Translation of Last Object Selected
-        sel = cmds.ls(sl=True)[len((cmds.ls(sl=True)))-1]
-        for a in ['X', 'Y', 'Z']:
-            randnum = random.uniform(min_range, max_range)
-            cmds.setAttr('{}.translate{}'.format(sel, a), randnum)
-        pass
+def function_wrapper(fn, *args, **kwargs):
+    def wrapped(_):
+        fn(*args, **kwargs)
+        
+    return wrapped
     
-    def randomize_scale_all():
-        #Randomizes Scale of All Selected objects
-        for i in cmds.ls(sl=True):
-            for a in ['X', 'Y', 'Z']:
-                randnum = random.uniform(min_range, max_range)
-                cmds.setAttr('{}.scale{}'.format(i, a), randnum)
-        pass
-                    
-    def randomize_scale():
-        #Randomizes Scale of Last Object Selected
-        sel = cmds.ls(sl=True)[len((cmds.ls(sl=True)))-1]
+
+def randomize_translate_all(min_range, max_range):
+    #Randomizes Translations of All Selected objects
+    for i in cmds.ls(sl=True):
         for a in ['X', 'Y', 'Z']:
             randnum = random.uniform(min_range, max_range)
-            cmds.setAttr('{}.scale{}'.format(sel, a), randnum)
-        pass
-                
-    def randomize_rotation_all():
-        #Randomizes Rotation of All Selected objects
-        for i in cmds.ls(sl=True):
-            for a in ['X', 'Y', 'Z']:
-                randnum = random.uniform(min_range, max_range)
-                cmds.setAttr('{}.rotate{}'.format(i, a), randnum)
-        pass
-                    
-    def randomize_rotation():
-        #Randomizes Rotation of Last Object Selected
-        sel = cmds.ls(sl=True)[len((cmds.ls(sl=True)))-1]
+            cmds.setAttr('{}.translate{}'.format(i, a), randnum)
+
+                  
+def randomize_translate(min_range, max_range):
+    #Randomizes Translation of Last Object Selected
+    sel = cmds.ls(sl=True)[len((cmds.ls(sl=True)))-1]
+    for a in ['X', 'Y', 'Z']:
+        randnum = random.uniform(min_range, max_range)
+        cmds.setAttr('{}.translate{}'.format(sel, a), randnum)
+
+    
+def randomize_scale_all(min_range, max_range):
+    #Randomizes Scale of All Selected objects
+    for i in cmds.ls(sl=True):
         for a in ['X', 'Y', 'Z']:
-            randnum = random.uniform((min_range, max_range)
-            cmds.setAttr('{}.rotate{}'.format(sel, a), randnum)
-        pass
+            randnum = random.uniform(min_range, max_range)
+            cmds.setAttr('{}.scale{}'.format(i, a), randnum)
+
+                    
+def randomize_scale(min_range, max_range):
+    #Randomizes Scale of Last Object Selected
+    sel = cmds.ls(sl=True)[len((cmds.ls(sl=True)))-1]
+    for a in ['X', 'Y', 'Z']:
+        randnum = random.uniform(min_range, max_range)
+        cmds.setAttr('{}.scale{}'.format(sel, a), randnum)
+
                 
-    if function_title == "Translate All":
-        return randomize_translate_all
-    else if function_title == "Translate Selected":
-        return randomize_translate
-    else if function_title == "Scale All":
-        return randomize_scale_all
-    else if function_title == "Scale Selected":
-        return randomize_scale
-    else if function_title == "Rotate All":
-        return randomize_rotation_all
-    else:
-        return randomize_rotation
-                    
-                    
+def randomize_rotation_all(min_range, max_range):
+    #Randomizes Rotation of All Selected objects
+    for i in cmds.ls(sl=True):
+        for a in ['X', 'Y', 'Z']:
+            randnum = random.uniform(min_range, max_range)
+            cmds.setAttr('{}.rotate{}'.format(i, a), randnum)
+
+
+def randomize_rotation(min_range, max_range):
+    #Randomizes Rotation of Last Object Selected
+    sel = cmds.ls(sl=True)[len((cmds.ls(sl=True)))-1]
+    for a in ['X', 'Y', 'Z']:
+        randnum = random.uniform(min_range, max_range)
+        cmds.setAttr('{}.rotate{}'.format(sel, a), randnum)               
+
+
 def randomization_ui():
     window = cmds.window(title = "My UI Test", widthHeight = (500,250), sizeable = True)
     
     master_layout = cmds.columnLayout(adjustableColumn=True)
     
-    #Scale Buttons/TextFields
     cmds.rowLayout(adjustableColumn=1, numberOfColumns=6)
         
-    scale_min_range_textfield = cmds.textField(editable=False, text='Minimum Range')
-    scale_min_range = cmds.textField(editable=True, text='1')
+    min_range_textfield = cmds.textField(editable=False, text='Minimum Range')
+    min_range = cmds.textField(editable=True, text='1')
         
-    scale_max_range_textfield = cmds.textField(editable=False, text='Max Range')
-    scale_max_range = cmds.textField(editable=True, text='10')
+    max_range_textfield = cmds.textField(editable=False, text='Max Range')
+    max_range = cmds.textField(editable=True, text='10')
         
     cmds.setParent(master_layout)
     
-    cmds.button(label = 'Randomize Scale', command=wrap_function("Translate All", min_range, max_range))
-    cmds.button(label = 'Randomize Scale for All Selected', command=wrap_function("Translate All", min_range, max_range))
+    cmds.button(label = 'Randomize Scale', command=function_wrapper(randomize_scale, min_range, max_range))
+    cmds.button(label = 'Randomize Scale for All Selected', command=function_wrapper(randomize_scale_all, min_range, max_range))
     
-    cmds.button(label = 'Randomize Rotation', command=wrap_function("Translate All", min_range, max_range))
-    cmds.button(label = 'Randomize Rotation for All Selected', command=wrap_function("Translate All", min_range, max_range))
+    cmds.button(label = 'Randomize Rotation', command=function_wrapper(randomize_rotation, min_range, max_range))
+    cmds.button(label = 'Randomize Rotation for All Selected', command=function_wrapper(randomize_rotation_all, min_range, max_range))
     
-    cmds.button(label = 'Randomize Translation', command=wrap_function("Translate All", min_range, max_range))
-    cmds.button(label = 'Randomize Translation for All Selected', command=wrap_function("Translate All", min_range, max_range))
+    cmds.button(label = 'Randomize Translation', command=function_wrapper(randomize_translate, min_range, max_range))
+    cmds.button(label = 'Randomize Translation for All Selected', command=function_wrapper(randomize_translate_all, min_range, max_range))
     
     
     cmds.showWindow(window)
